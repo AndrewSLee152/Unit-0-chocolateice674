f8189138d3009f53d4cc5db4fbce51a18a1a2dd3
Andrew Lee <leeandrew152@gmail.com>
2016-10-03 12:35:26 -0700

    Add files via upload

---
+++ Rick Roll.java
@@ -0,0 +1,18 @@
+public class rickroll{
+  public static void melody(){
+   System.out.print("Never gonna give you up") ;
+        System.out.println("Never gonna let you down") ;
+        System.out.println("Never gonna run around and desert you") ;
+     System.out.println("Never gonna make you cry, never gonna say goodbye");
+                        System.out.println("Never gonna tell a lie and hurt you");
+  }
+  public static void main(String[] args){
+    System.out.println("We're no strangers to love \nYou know the rules and so do I \nA full commitment's what I'm thinking of \nYou wouldn't get this from any other guy \nI just want to tell you how I'm feeling \nGotta make you understand");
+    melody();
+    System.out.println("We've known each other for so long \nYour heart's been aching but you're too shy to say it \nInside we both know what's been going on \nWe know the game and we're gonna play it \nAnd if you ask me how I'm feeling \nDon't tell me you're too blind to see");
+ melody();
+    melody();
+    System.out.println("We've known each other for so long \nYour heart's been aching but you're too shy to say it \nInside we both know what's been going on \nWe know the game and we're gonna play it \nI just want to tell you how I'm feeling \nGotta make you understand");
+melody();
+  }
+}
